ff82e5566dba6fe26a54b3bb67b6e19e8f845462
Duhan <158033724+DuhanDemirGameDev@users.noreply.github.com>
2026-04-16 14:43:10 +0300

    Git init

---
+++ backend/GitSocial/src/main/java/com/example/gitsocial/GitSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSocialApplication.class, args);
+    }
+
+}
---
+++ backend/GitSocial/src/test/java/com/example/gitsocial/GitSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
